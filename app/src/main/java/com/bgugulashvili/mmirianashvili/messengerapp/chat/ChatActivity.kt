49c14aca41c08d552ce530e5e98609b3083dae0c
mirian_mirianashvili <mmiri17@freeuni.edu.ge>
2021-08-15 22:30:20 +0400

    added initial chat view

---
+++ app/src/main/java/com/bgugulashvili/mmirianashvili/messengerapp/chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.bgugulashvili.mmirianashvili.messengerapp.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bgugulashvili.mmirianashvili.messengerapp.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
